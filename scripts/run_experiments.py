61211413416a56b17a1bb36390a46a4ee2690d59
Taylor Shin <taylorshin12@gmail.com>
2020-05-21 22:30:30 -0700

    add code to eval_generation that evaluates BERT predictions given trigger based prompts

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -15,39 +15,10 @@ import os
 from os.path import isfile, join
 from shutil import copyfile
 from collections import defaultdict
-
 from operator import itemgetter
+import random
 
 LMs = [
-    # {
-    #     "lm":
-    #     "transformerxl",
-    #     "label":
-    #     "transformerxl",
-    #     "models_names": ["transformerxl"],
-    #     "transformerxl_model_name":
-    #     'transfo-xl-wt103',
-    #     "transformerxl_model_dir":
-    #     "pre-trained_language_models/transformerxl/transfo-xl-wt103/"
-    # },
-    # {
-    #     "lm": "elmo",
-    #     "label": "elmo",
-    #     "models_names": ["elmo"],
-    #     "elmo_model_name": 'elmo_2x4096_512_2048cnn_2xhighway',
-    #     "elmo_vocab_name": 'vocab-2016-09-10.txt',
-    #     "elmo_model_dir": "pre-trained_language_models/elmo/original",
-    #     "elmo_warm_up_cycles": 10
-    # },
-    #     {
-    #     "lm": "elmo",
-    #     "label": "elmo5B",
-    #     "models_names": ["elmo"],
-    #     "elmo_model_name": "elmo_2x4096_512_2048cnn_2xhighway_5.5B",
-    #     "elmo_vocab_name": "vocab-enwiki-news-500000.txt",
-    #     "elmo_model_dir": "pre-trained_language_models/elmo/original5.5B/",
-    #     "elmo_warm_up_cycles": 10
-    # },
     {
         "lm": "bert",
         "label": "bert_base",
@@ -91,10 +62,6 @@ def run_experiments(
     for relation in relations:
         pp.pprint(relation)
         PARAMETERS = {
-            # ConceptNet, etc
-            # "dataset_filename": "{}{}{}".format(
-            #     data_path_pre, relation["relation"], data_path_post
-            # ),
             # Google RE and TREx
             "dataset_filename": "{}/{}/{}".format(
                 data_path_pre, relation["relation"], data_path_post
@@ -102,7 +69,7 @@ def run_experiments(
             "common_vocab_filename": "pre-trained_language_models/common_vocab_cased.txt",
             "template": "",
             "bert_vocab_name": "vocab.txt",
-            "batch_size": 64,
+            "batch_size": 32, # NOTE: 64 for UNCONDITIONAL and 32 for CONDITIONAL
             "logdir": "output",
             "full_logdir": "output/results/{}/{}".format(
                 input_param["label"], relation["relation"]
@@ -112,7 +79,7 @@ def run_experiments(
             "threads": -1,
             "interactive": False,
             "use_negated_probes": use_negated_probes,
-            "use_context": False,
+            "use_ctx": True,
             "synthetic": False
         }
 
@@ -157,7 +124,7 @@ def run_experiments(
         if model is None:
             [model_type_name] = args.models_names
             model = build_model_by_name(model_type_name, args)
-        MRR, Precision, Precision1 = run_evaluation(args, shuffle_data=False, model=model, use_context=args.use_context, synthetic=args.synthetic)
+        MRR, Precision, Precision1 = run_evaluation(args, shuffle_data=False, model=model, use_ctx=args.use_ctx, synthetic=args.synthetic)
         print("P@1 : {}".format(Precision1), flush=True)
         all_Precision1.append(Precision1)
 
@@ -198,7 +165,7 @@ def run_experiments(
 def get_TREx_parameters(data_path_post, data_path_pre="data/"):
     relations = load_file("{}relations.jsonl".format(data_path_pre))
     # data_path_pre += "TREx/"
-    data_path_pre = "data/LMAT/TREx_all_D"
+    data_path_pre = "data/LMAT/TREx_R_bench_cond"
     # data_path_post = "train.jsonl"
     # data_path_post = "val.jsonl"
     # data_path_post = "test.jsonl"
@@ -231,29 +198,11 @@ def get_GoogleRE_parameters():
     return relations, data_path_pre, data_path_post
 
 
-def get_ConceptNet_parameters(data_path_post, data_path_pre="data/"):
-    """
-    relations = [{"relation": "test"}]
-    data_path_pre += "ConceptNet/"
-    data_path_post = ".jsonl"
-    return relations, data_path_pre, data_path_post
-    """
-    relations = load_file("{}relations_concept.jsonl".format(data_path_pre))
-    data_path_pre = 'data/LMAT/ConceptNet'
-    return relations, data_path_pre, data_path_post
-
-
-def get_Squad_parameters(data_path_pre="data/"):
-    relations = [{"relation": "test"}]
-    data_path_pre += "Squad/"
-    data_path_post = ".jsonl"
-    return relations, data_path_pre, data_path_post
-
-
 def run_all_LMs(parameters, metrics, dataset_type):
     for ip in LMs:
         print(ip["label"])
         run_experiments(*parameters, metrics, dataset_type, input_param=ip, use_negated_probes=False)
+
 
 def print_all_relation_metrics(metrics):
     for relation in metrics:
@@ -273,6 +222,7 @@ def print_all_relation_metrics(metrics):
 
 
 if __name__ == "__main__":
+
     # print("1. Google-RE")    
     # parameters = get_GoogleRE_parameters()
     # run_all_LMs(parameters)
@@ -293,25 +243,3 @@ if __name__ == "__main__":
     run_all_LMs(parameters, metrics, 'test')
     print(('='*40) + ' METRICS ' + ('='*40))
     print_all_relation_metrics(metrics)
-
-    # print("3. ConceptNet")
-    # metrics = {}
-    # print(('='*40) + ' TRAIN ' + ('='*40))
-    # data_path_post = 'train.jsonl'
-    # parameters = get_ConceptNet_parameters(data_path_post)
-    # run_all_LMs(parameters, metrics, 'train')
-    # print(('='*40) + ' DEV ' + ('='*40))
-    # data_path_post = 'dev.jsonl'
-    # parameters = get_ConceptNet_parameters(data_path_post)
-    # run_all_LMs(parameters, metrics, 'dev')
-    # print(('='*40) + ' TEST ' + ('='*40))
-    # data_path_post = 'test.jsonl'
-    # parameters = get_ConceptNet_parameters(data_path_post)
-    # run_all_LMs(parameters, metrics, 'test')
-    # print(('='*40) + ' METRICS ' + ('='*40))
-    # print_all_relation_metrics(metrics)
-
-    # print("4. SQuAD")
-    # parameters = get_Squad_parameters()
-    # run_all_LMs(parameters)
-
